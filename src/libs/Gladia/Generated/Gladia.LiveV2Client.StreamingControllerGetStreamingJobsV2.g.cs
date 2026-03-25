77b0c53378ab5eb645cd3e61433ae4b81836c50e
Konstantin S. <havendv@gmail.com>
2026-03-25 06:34:18 +0400

    feat: Updated OpenAPI spec (#22)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Gladia/Generated/Gladia.LiveV2Client.StreamingControllerGetStreamingJobsV2.g.cs
+++ src/libs/Gladia/Generated/Gladia.LiveV2Client.StreamingControllerGetStreamingJobsV2.g.cs
@@ -43,7 +43,7 @@ namespace Gladia
         /// Default Value: 20
         /// </param>
         /// <param name="date">
-        /// Example: 2026-03-24
+        /// Example: 2026-03-25
         /// </param>
         /// <param name="beforeDate">
         /// Example: 2024-01-01T00:00:00.000Z

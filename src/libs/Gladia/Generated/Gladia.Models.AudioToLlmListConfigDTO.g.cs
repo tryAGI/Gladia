f53bc57183b136afc566375fb69d3abb3eed3300
Konstantin S. <havendv@gmail.com>
2026-03-31 20:08:27 +0400

    feat: Updated OpenAPI spec (#36)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Gladia/Generated/Gladia.Models.AudioToLlmListConfigDTO.g.cs
+++ src/libs/Gladia/Generated/Gladia.Models.AudioToLlmListConfigDTO.g.cs
@@ -18,6 +18,15 @@ namespace Gladia
         public required global::System.Collections.Generic.IList<byte[]> Prompts { get; set; }
 
         /// <summary>
+        /// The model to use for the prompt execution. You can find the list of supported models [here](https://openrouter.ai/models).<br/>
+        /// Default Value: openai/gpt-3.5-turbo
+        /// </summary>
+        /// <default>"openai/gpt-3.5-turbo"</default>
+        [global::System.Text.Json.Serialization.JsonPropertyName("model")]
+        [global::System.Text.Json.Serialization.JsonRequired]
+        public required string Model { get; set; } = "openai/gpt-3.5-turbo";
+
+        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -30,13 +39,19 @@ namespace Gladia
         /// The list of prompts applied on the audio transcription<br/>
         /// Example: [Extract the key points from the transcription]
         /// </param>
+        /// <param name="model">
+        /// The model to use for the prompt execution. You can find the list of supported models [here](https://openrouter.ai/models).<br/>
+        /// Default Value: openai/gpt-3.5-turbo
+        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public AudioToLlmListConfigDTO(
-            global::System.Collections.Generic.IList<byte[]> prompts)
+            global::System.Collections.Generic.IList<byte[]> prompts,
+            string model)
         {
             this.Prompts = prompts ?? throw new global::System.ArgumentNullException(nameof(prompts));
+            this.Model = model ?? throw new global::System.ArgumentNullException(nameof(model));
         }
 
         /// <summary>

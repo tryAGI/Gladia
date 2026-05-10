bbfa33860330a832e736912a7e52b4be446e687a
Konstantin S. <havendv@gmail.com>
2026-05-10 07:38:56 +0400

    feat: Updated OpenAPI spec (#95)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Gladia/Generated/Gladia.JsonConverters.OneOf2.g.cs
+++ src/libs/Gladia/Generated/Gladia.JsonConverters.OneOf2.g.cs
@@ -107,7 +107,10 @@ namespace Gladia.JsonConverters
                 catch (global::System.InvalidOperationException)
                 {
                 }
+            }
 
+            if (value1 == null && value2 == null)
+            {
                 try
                 {
 

dd8e6cc12e198e7aed57992ac100a0196cc1abd9
Konstantin S. <havendv@gmail.com>
2026-06-11 08:17:04 +0400

    feat: Updated OpenAPI spec (#133)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Gladia/Generated/Gladia.Models.SentencesDTO.g.cs
+++ src/libs/Gladia/Generated/Gladia.Models.SentencesDTO.g.cs
@@ -39,8 +39,7 @@ namespace Gladia
         /// If `sentences` has been enabled, transcription as sentences.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("results")]
-        [global::System.Text.Json.Serialization.JsonRequired]
-        public required global::System.Collections.Generic.IList<string> Results { get; set; }
+        public global::System.Collections.Generic.IList<string>? Results { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -60,11 +59,11 @@ namespace Gladia
         /// <param name="execTime">
         /// Time audio intelligence model took to complete the task
         /// </param>
-        /// <param name="results">
-        /// If `sentences` has been enabled, transcription as sentences.
-        /// </param>
         /// <param name="error">
         /// `null` if `success` is `true`. Contains the error details of the failed model
+        /// </param>
+        /// <param name="results">
+        /// If `sentences` has been enabled, transcription as sentences.
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
@@ -73,14 +72,14 @@ namespace Gladia
             bool success,
             bool isEmpty,
             double execTime,
-            global::System.Collections.Generic.IList<string> results,
-            global::Gladia.AddonErrorDTO? error)
+            global::Gladia.AddonErrorDTO? error,
+            global::System.Collections.Generic.IList<string>? results)
         {
             this.Success = success;
             this.IsEmpty = isEmpty;
             this.ExecTime = execTime;
             this.Error = error;
-            this.Results = results ?? throw new global::System.ArgumentNullException(nameof(results));
+            this.Results = results;
         }
 
         /// <summary>

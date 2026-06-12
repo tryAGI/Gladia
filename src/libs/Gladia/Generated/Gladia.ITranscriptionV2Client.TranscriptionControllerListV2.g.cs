b3d88a6d5c5f861c942084b67d030103759b1015
Konstantin S. <havendv@gmail.com>
2026-06-12 14:19:34 +0400

    feat: Updated OpenAPI spec (#137)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Gladia/Generated/Gladia.ITranscriptionV2Client.TranscriptionControllerListV2.g.cs
+++ src/libs/Gladia/Generated/Gladia.ITranscriptionV2Client.TranscriptionControllerListV2.g.cs
@@ -14,7 +14,7 @@ namespace Gladia
         /// Default Value: 20
         /// </param>
         /// <param name="date">
-        /// Example: 2026-06-11
+        /// Example: 2026-06-12
         /// </param>
         /// <param name="beforeDate">
         /// Example: 2024-01-01T00:00:00.000Z
@@ -55,7 +55,7 @@ namespace Gladia
         /// Default Value: 20
         /// </param>
         /// <param name="date">
-        /// Example: 2026-06-11
+        /// Example: 2026-06-12
         /// </param>
         /// <param name="beforeDate">
         /// Example: 2024-01-01T00:00:00.000Z

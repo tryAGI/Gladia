2a6e3218faf5fdd8450937e98b0ea40026b94c8c
Konstantin S. <havendv@gmail.com>
2026-09-13 19:51:33 +0400

    feat: Updated OpenAPI spec (#258)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Gladia/Generated/Gladia.PreRecordedV2Client.Authorizations.ApiKeyInHeader.g.cs
@@ -0,0 +1,36 @@
+
+#nullable enable
+
+namespace Gladia
+{
+    public sealed partial class PreRecordedV2Client
+    {
+
+        /// <inheritdoc/>
+        public void AuthorizeUsingApiKeyInHeader(
+            string apiKey)
+        {
+            apiKey = apiKey ?? throw new global::System.ArgumentNullException(nameof(apiKey));
+
+            for (var i = Authorizations.Count - 1; i >= 0; i--)
+            {
+                var __authorization = Authorizations[i];
+                if (__authorization.Type == "ApiKey" &&
+                    __authorization.Location == "Header" &&
+                    __authorization.Name == "x-gladia-key")
+                {
+                    Authorizations.RemoveAt(i);
+                }
+            }
+
+            Authorizations.Add(new global::Gladia.EndPointAuthorization
+            {
+                Type = "ApiKey",
+                SchemeId = "ApikeyXGladiaKey",
+                Location = "Header",
+                Name = "x-gladia-key",
+                Value = apiKey,
+            });
+        }
+    }
+}
